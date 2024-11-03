34f79ed789a116d2208501322c5307ed7376d179
kkaaars <vadim.holuysev@gmail.com>
2024-11-04 00:16:42 +0300

    second commit

--- Lab1/build/Desktop_x86_windows_msvc2022_pe_64bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Colors_FILE D:/Progs/Qt/QtProjects/Lab1/build/Desktop_x86_windows_msvc2022_pe_64bit-Release/Colors.exe)
-set(__QT_DEPLOY_TARGET_Colors_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Colors_RUNTIME_DLLS D:/Progs/Qt/qttq/6.8.0/msvc2022_64/bin/Qt6Widgets.dll;D:/Progs/Qt/qttq/6.8.0/msvc2022_64/bin/Qt6Gui.dll;D:/Progs/Qt/qttq/6.8.0/msvc2022_64/bin/Qt6Core.dll)
